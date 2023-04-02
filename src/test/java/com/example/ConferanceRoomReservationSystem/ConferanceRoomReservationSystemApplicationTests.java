b97ba20d6d3e90061970af5d5a102a82a7e0489f
Jakub Rutkowski <kubol0075@gmail.com>
2023-04-02 17:48:51 +0200

    first commit

---
+++ src/test/java/com/example/ConferanceRoomReservationSystem/ConferanceRoomReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConferanceRoomReservationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferanceRoomReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
