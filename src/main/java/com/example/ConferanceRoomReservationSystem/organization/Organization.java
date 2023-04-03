d063ab9efac535315dae4f4baf9414a286f13579
Jakub Rutkowski <kubol0075@gmail.com>
2023-04-03 21:54:17 +0200

    test(crrs):add OrganizationRepository unit tests

--- src/main/java/com/example/ConferanceRoomReservationSystem/organization/Organization.java
+++ src/main/java/com/example/ConferanceRoomReservationSystem/organization/Organization.java
@@ -86,4 +86,25 @@ public class Organization {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    @Override
+    public String toString() {
+        return "Organization{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Organization that = (Organization) o;
+        return Objects.equals(name, that.name) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description);
+    }
 }
